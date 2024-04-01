2c6086ea5b35542c5f092d93ee58ca0386bd894d
amorajoaovictor <amorajoaovictor2@gmail.com>
2024-04-01 14:43:56 -0300

    Update form.css, form.html, index.html, script.js, and style.css

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const titulo = document.querySelector('.digitando');
+
+	if (titulo) {
+		ativaletra(titulo);
+	} else {
+		console.error("Elemento com a classe 'digitando' não foi encontrado.");
+	}
+
+	function ativaletra(elemento) {
+		const arrTexto = elemento.innerHTML.split('');
+		elemento.innerHTML = '';
+		let i = 0;
+
+		const interval = setInterval(() => {
+			if (i < arrTexto.length) {
+				elemento.innerHTML += arrTexto[i];
+				i++;
+			} else {
+				clearInterval(interval);
+			}
+		}, 75);
+	}
+});
